bef8de7dea5b11d10da1815a3f531e3cf4dd14db
JOSE ANGEL LUGO ESPARZA <lugo.joseangel.c66@gmail.com>
2025-04-07 09:29:51 -0600

    Configuracion de swagger para acepte tokens

--- MesaCore/Program.cs
+++ MesaCore/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,7 +16,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+   options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+   {
+       Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+       Name = "Authorization",
+       In = ParameterLocation.Header,
+       Type = SecuritySchemeType.ApiKey,
+       Scheme = "Bearer"
+   });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 
 builder.Services.AddSingleton<AzureStorageService>();
