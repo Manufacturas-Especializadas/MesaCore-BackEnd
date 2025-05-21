d6e9bc512a8c0a18b7ec644b91377810f3cedbb8
Angel Esparza <98989221+Angel02J@users.noreply.github.com>
2025-05-21 08:37:59 -0600

    Update EmailServices.cs

--- MesaCore/Services/EmailServices.cs
+++ MesaCore/Services/EmailServices.cs
@@ -14,33 +14,32 @@ namespace MesaCore.Services
             _emailSettings = emailSettings.Value;
         }
 
-        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        public async Task SendEmailAsync(string to, string subject, string bodyHtml)
         {
             using (var smtpClient = new SmtpClient(_emailSettings.SmtpServer, _emailSettings.Port))
             {
                 smtpClient.Credentials = new NetworkCredential(_emailSettings.SenderEmail, _emailSettings.Password);
                 smtpClient.EnableSsl = true;
-
+        
                 var mailMessage = new MailMessage()
                 {
                     From = new MailAddress(_emailSettings.SenderEmail, _emailSettings.SenderName),
                     Subject = subject,
-                    Body = body,
+                    Body = bodyHtml,
                     IsBodyHtml = true
                 };
-
-                mailMessage.To.Add(toEmail);
-
+        
+                mailMessage.To.Add(to);
+        
                 try
                 {
                     await smtpClient.SendMailAsync(mailMessage);
                 }
                 catch (SmtpException ex)
-                {                     
-                    throw new Exception("Error al enviar el correo: " + ex.Message);
+                {
+                    throw new Exception($"Error al enviar el correo: {ex.Message}");
                 }
             }
-        }
-
+        }      
     }
 }
