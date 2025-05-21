70287612daf4ad2cf477a413436096279a725063
Angel Esparza <98989221+Angel02J@users.noreply.github.com>
2025-05-21 08:36:57 -0600

    Update EmailSettings.cs

--- MesaCore/Data/EmailSettings.cs
+++ MesaCore/Data/EmailSettings.cs
@@ -5,12 +5,13 @@
         public string SmtpServer { get; set; }
 
         public int Port { get; set; }
-
+        
         public string SenderEmail { get; set; }
-
-        public string SenderName { get; set; }
-
+        
         public string Password { get; set; }
-
+        
+        public bool EnableSs1 { get; set; }
+        
+        public string SenderName { get; set; }
     }
 }
