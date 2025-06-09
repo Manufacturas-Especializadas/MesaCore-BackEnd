2bdb1374030bdba8b8f8578d545873b2b26defd1
JOSE ANGEL LUGO ESPARZA <lugo.joseangel.c66@gmail.com>
2025-06-09 11:49:51 -0600

    Dtos de seccion de proyectos de AL y CU

---
+++ MesaCore/Dtos/ProyectoUpdateCUDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MesaCore.Dtos
+{
+    public class ProyectoUpdateCUDto
+    {
+        public string NombreDelProyecto { get; set; }
+        public int SolicitanteId { get; set; }
+        public int PlantaId { get; set; }
+        public int EstatusId { get; set; }
+    }
+}
---
+++ MesaCore/Dtos/ProyectoUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MesaCore.Dtos
+{
+    public class ProyectoUpdateDto
+    {
+        public string NombreDelProyecto { get; set; }
+        public int SolicitanteId { get; set; }
+        public int PlantaId { get; set; }
+        public int EstatusId { get; set; }
+
+    }
+}
